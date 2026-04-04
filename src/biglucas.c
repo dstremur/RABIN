950d8580a6af7e4f14a19c53ce286d96cafe3c0c
Diego <diego@strebel.com>
2026-04-04 22:30:56 +0200

    iterative lucas sequence

--- src/biglucas.c
+++ src/biglucas.c
@@ -78,8 +78,8 @@ void bn_lucas_solve(bignum* u, bignum* v, bignum* p, bignum* q, bignum* qn,
   bn_free(&tmp);
 }
 
-void bn_lucas_solve_mod(bignum* u, bignum* v, bignum* p, bignum* q, bignum* qn,
-                        bignum* n, bignum* m)
+void bn_lucas_solve_mod_old(bignum* u, bignum* v, bignum* p, bignum* q,
+                            bignum* qn, bignum* n, bignum* m)
 {
   if (bn_is_eq_i64(n, 0)) {
     bn_set_u64(u, 0);
@@ -161,6 +161,95 @@ void bn_lucas_solve_mod(bignum* u, bignum* v, bignum* p, bignum* q, bignum* qn,
   bn_free(&u_n);
   bn_free(&v_n);
   bn_free(&q_n);
+  bn_free(&a);
+  bn_free(&b);
+  bn_free(&tmp);
+}
+
+void bn_lucas_solve_mod(bignum* u, bignum* v, bignum* p, bignum* q, bignum* qn,
+                        bignum* n, bignum* m)
+{
+  if (bn_is_eq_i64(n, 0)) {
+    bn_set_u64(u, 0);
+    bn_set_u64(v, 2);
+    bn_set_u64(qn, 1);
+    return;
+  }
+
+  if (bn_is_eq_i64(n, 1)) {
+    bn_set_u64(u, 1);
+    bn_copy(v, p);
+    bn_copy(qn, q);
+
+    bn_mod(v, v, m);
+    bn_mod(qn, qn, m);
+    return;
+  }
+
+  bignum a, b, tmp;
+  bn_init_multi(&a, &b, &tmp, NULL);
+
+  u64 len = bn_bit_length(n);
+
+  bn_set_u64(u, 1);
+  bn_copy(v, p);
+  bn_copy(qn, q);
+
+  for (i64 i = len - 2; i >= 0; i--) {
+    // Precompute used values
+    bn_mul(&a, u, v);
+    bn_mul(&b, v, v);
+
+    bn_mod(&a, &a, m);
+    bn_mod(&b, &b, m);
+
+    if (bn_get_bit(n, i) == 0) {
+      // U_2n = a
+      bn_copy(u, &a);
+      // V_2n = b - 2 * Q^k
+      bn_copy(v, &b);
+      bn_add(v, v, m);
+      bn_sub(v, v, qn);
+      bn_add(v, v, m);
+      bn_sub(v, v, qn);
+      bn_mod(v, v, m);
+      // Q^n = (Q^n/2)^2
+      bn_mul(qn, qn, qn);
+      bn_mod(qn, qn, m);
+    } else {
+      bn_mul(&tmp, p, &a);
+      bn_add(&tmp, &tmp, &b);
+      // fix for modular arethmetic
+      if (!bn_is_even(&tmp)) {
+        bn_add(&tmp, &tmp, m);
+      }
+      bn_rshift1(&tmp);
+
+      bn_add(&tmp, &tmp, m);
+      bn_sub(u, &tmp, qn);
+      bn_mod(u, u, m);
+
+      bn_mul(v, p, u);
+      bn_mod(v, v, m);
+
+      bn_mul(&tmp, q, &a);
+      bn_mod(&tmp, &tmp, m);
+
+      bn_add(v, v, m);
+      bn_sub(v, v, &tmp);
+      bn_mod(v, v, m);
+
+      bn_add(v, v, m);
+      bn_sub(v, v, &tmp);
+      bn_mod(v, v, m);
+
+      bn_mul(qn, qn, qn);
+      bn_mod(qn, qn, m);
+      bn_mul(qn, qn, q);
+      bn_mod(qn, qn, m);
+    }
+  }
+
   bn_free(&a);
   bn_free(&b);
   bn_free(&tmp);
--- src/bigmont.c
+++ src/bigmont.c
@@ -45,10 +45,11 @@ void bn_mont_ctx_init(bn_mont_ctx* ctx, bignum* n)
   }
 }
 
-void bn_mont_ctx_free(bn_mont_ctx *ctx) {
-	bn_free(&ctx->one_mont);
-	bn_free(&ctx->n);
-	bn_free(&ctx->r_square);
+void bn_mont_ctx_free(bn_mont_ctx* ctx)
+{
+  bn_free(&ctx->one_mont);
+  bn_free(&ctx->n);
+  bn_free(&ctx->r_square);
 }
 
 void bn_mont_redc(bignum* r, bignum* t, bn_mont_ctx* ctx)
