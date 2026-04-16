c848818b0a5504dcb544c139277b7591859d10f3
Diego <diego@strebel.com>
2026-04-16 20:44:10 +0200

    unroll loop 4 times

--- src/bn_mul_inner.asm
+++ src/bn_mul_inner.asm
@@ -1,46 +1,83 @@
 ; Target: x86_64, Intel Syntax
-; ABI: System V -> rdi=r, rsi=b, rdx=a_limb, rcx=len
+; ABI: System V -> rdi = r, rsi = b, rdx = a_limb, rcx = len
 
 section .text
 global bn_mul_add_inner
 
 bn_mul_add_inner:
-    test rcx, rcx
-    jz .done_early
+    test    rcx, rcx
+    jz      .done_early
 
-    xor r8, r8      ; r8 will hold our Previous High Limb
+    mov     rax, rcx
+    and     rax, 3          ; remainder = len % 4
+    shr     rcx, 2          ; blocks = len / 4
 
-    align 16        ; Align loop for instruction cache
+    xor     r8, r8          ; carry from previous mulx high part
+
+    xor     r9, r9
+    adcx    r9, r9          ; CF = 0
+    adox    r9, r9          ; OF = 0
+
+	test rcx, rcx
+    jz   .tail_setup
+
+align 16
 .loop:
-    ; mulx: r11:r10 = a_limb * b[i]
-    mulx r11, r10, [rsi]
+    ; limb 0
+    mulx    r11, r10, [rsi]
+    adcx    r10, r8
+    adox    r10, [rdi]
+    mov     [rdi], r10
+    mov     r8, r11
 
-    ; Chain 1 (CF): Add previous high limb to current low limb
-    adcx r10, r8
-    
-    ; Chain 2 (OF): Add destination limb to current low limb
-    adox r10, [rdi]
-    
-    ; Store low limb and carry the new high limb to the next iteration
-    mov [rdi], r10
-    mov r8, r11
-    
-    ; Advance pointers
-    lea rdi, [rdi + 8]
-    lea rsi, [rsi + 8]
+    ; limb 1
+    mulx    r11, r10, [rsi + 8]
+    adcx    r10, r8
+    adox    r10, [rdi + 8]
+    mov     [rdi + 8], r10
+    mov     r8, r11
 
-    ; THE MAGIC LOOP:
-    ; 'lea' decrements rcx without touching CF or OF!
-    ; 'jrcxz' jumps out if rcx == 0 without touching CF or OF!
-	loop .loop
+    ; limb 2
+    mulx    r11, r10, [rsi + 16]
+    adcx    r10, r8
+    adox    r10, [rdi + 16]
+    mov     [rdi + 16], r10
+    mov     r8, r11
+
+    ; limb 3
+    mulx    r11, r10, [rsi + 24]
+    adcx    r10, r8
+    adox    r10, [rdi + 24]
+    mov     [rdi + 24], r10
+    mov     r8, r11
+
+    lea     rdi, [rdi + 32]
+    lea     rsi, [rsi + 32]
+
+    loop    .loop
+
+.tail_setup:
+    mov rcx, rax	; switch to rcx for loop to work 
+    jrcxz  .done
+
+.tail_loop:
+    mulx    r11, r10, [rsi]
+    adcx    r10, r8
+    adox    r10, [rdi]
+    mov     [rdi], r10
+    mov     r8, r11
+
+    lea     rdi, [rdi + 8]
+    lea     rsi, [rsi + 8]
+
+    loop    .tail_loop
 
 .done:
-    ; Finalize the carries. 
-    ; adcx and adox do NOT interfere with each other's flags!
-    mov r10, 0
-    adcx r8, r10   ; Fold in remaining CF
-    adox r8, r10    ; Fold in remaining OF
-    add [rdi], r8
+    mov     r10, 0
+    adcx    r8, r10
+    adox    r8, r10
+    add     [rdi], r8
+
 .done_early:
     ret
 
