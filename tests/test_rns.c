1dc318c0a3e3d467acd6c32075b08c016dc2ad4c
Diego Strebel <diego@strebel.com>
2026-05-12 08:39:12 +0200

    add rns test

---
+++ tests/test_rns.c
@@ -0,0 +1,35 @@
+#include "../include/bigrns.h"
+
+#include <stdio.h>
+
+int main() {
+	// 1. Setup Context
+ctx_rns ctx;
+u64 primes[] = {0xFFFFFFFFFFFFFFC5ULL, 0xFFFFFFFFFFFFFF4DULL}; // Example 64-bit primes
+rns_context_init(&ctx, primes, 2);
+
+// 2. Prepare Numbers
+bignum n;
+bn_init(&n);
+bn_init_val(&n, "34534534058309509345030534589375935");
+
+rns_num r;
+r.residues = NULL; // bignum_to_rns will malloc if NULL
+
+// 3. Convert and Convert Back
+bignum_to_rns(&r, &n, &ctx);
+bignum reconstructed;
+bn_init(&reconstructed);
+
+rns_to_bignum(&reconstructed, &r, &ctx);
+
+// Now: reconstructed == my_big
+
+printf("original :");
+bn_println(&n);
+
+printf("rns :");
+bn_println(&reconstructed);
+
+bn_free_multi(&n, &reconstructed, NULL);
+}
